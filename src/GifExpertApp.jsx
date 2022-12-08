f971c01b53c8900047400a32197bc06fbb98fc7a
fml <lizarragafranco91@gmail.com>
2022-12-07 23:24:01 -0300

    Commit Inicial

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([ 'One Punch' ]);
+  const onAddCategory = ( newCategory ) => {
+    // categories.push('Slipknot');
+    if(categories.includes(newCategory)) return;
+    setCategories( [newCategory, ...categories] );
+  };
+  return (
+    <>
+        <h1>Gif Expert App</h1>
+        <AddCategory onSend={ onAddCategory } />
+          { categories.map( 
+            category => (
+                <GifGrid key={ category } category={ category } />
+            ) 
+          )}
+    </>
+  )
+}
